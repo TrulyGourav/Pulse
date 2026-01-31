d7ada1d82dbe70fcc65f93b552e7b94dac9cda4b
gourav.singh <gourav.singh@sophos.com>
2026-01-31 16:54:19 +0530

    added user entities

---
+++ auth-service/src/main/java/com/project/auth_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.auth_service.repository;
+
+import com.project.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
