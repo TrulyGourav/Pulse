42d8e3daff772f6301201216add59484881fcafc
gourav.singh <gourav.singh@sophos.com>
2026-02-01 11:15:04 +0530

    query service setup

---
+++ analytics-query-service/src/main/java/com/project/analytics_query_service/AnalyticsQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.analytics_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsQueryServiceApplication.class, args);
+	}
+
+}
---
+++ analytics-query-service/src/test/java/com/project/analytics_query_service/AnalyticsQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.analytics_query_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
