b27c7e69daa5ecc92f29b32847a234d7823f7eab
gourav.singh <gourav.singh@sophos.com>
2026-02-01 10:58:12 +0530

    processor setup

---
+++ analytics-processor-service/src/main/java/com/project/analytics_processor_service/AnalyticsProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.analytics_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsProcessorServiceApplication.class, args);
+	}
+
+}
